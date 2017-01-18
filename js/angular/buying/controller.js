e8bf05279358690cf6296e5abfa06d8af9ecee31
Aseef Ahmed <student@localhost.localdomain>
2017-01-18 09:07:49 +0600

    4

--- js/angular/buying/controller.js
+++ js/angular/buying/controller.js
@@ -1,23 +1,4 @@
 angular.module('myApp').controller('BuyingOrderController', function($scope, $http, $routeParams, $route, Upload){
-<<<<<<< HEAD
-		$scope.loginUser = JSON.parse(sessionStorage.getItem('loginUser'));
-        if($routeParams.order_id){
-            $scope.order_id = $routeParams.order_id;
-        }
-		$scope.host = app.host;
-		$scope.buying_orders = function () {
-	        $scope.page_title = 'Orders List';
-	        $scope.num_of_items_arr = [{id: 5, value: 5},{id: 10, value: 10},{id: 20, value: 20},{id: 50, value: 50},{id: 100, value: 100}];
-	        $http.get(app.host + 'buying/order/fetchOrdersList/'+$scope.loginUser.id+'/'+$scope.loginUser.emp_role).then(function (response) {
-	            console.log('ddd');
-	            console.log(response.data)
-	            $scope.num_of_items = 10;
-	            $scope.orders = response.data.orders;
-	            $scope.reverse = false;
-	        });
-	        $http.get(app.host + '/production/buyer/fetchBuyersList').then(function (response) {
-            $scope.buyers = response.data;
-=======
     $scope.loginUser = JSON.parse(sessionStorage.getItem('loginUser'));
     $scope.host = app.host;
     $scope.buying_orders = function () {
@@ -28,7 +9,7 @@ angular.module('myApp').controller('BuyingOrderController', function($scope, $ht
             console.log(response.data)
             $scope.num_of_items = 10;
             $scope.orders = response.data.orders;
->>>>>>> 0ad9bd16f39e053db77a6e9275e06207a360cd97
+            $scope.days_of_14_days = response.data.data_of_14_days_ago;
             $scope.reverse = false;
         });
         $http.get(app.host + '/production/buyer/fetchBuyersList').then(function (response) {
@@ -47,7 +28,7 @@ angular.module('myApp').controller('BuyingOrderController', function($scope, $ht
             val: 'No'
         }
     ]
-    
+
     $scope.buyersList = function()
     {
         $http.get(app.host + '/production/buyer/fetchBuyersList').then(function (response) {
@@ -67,45 +48,15 @@ angular.module('myApp').controller('BuyingOrderController', function($scope, $ht
 
     };
 
-    $scope.edit_buying_order_confirmed = function (field, id, value, data_type=null) {
+    $scope.edit_buying_order_confirmed = function(field, id, value, data_type = null) {
 
-<<<<<<< HEAD
-	    }
-		$scope.init = function(id){alert(id)
-	        $scope.page_title = 'Order Details';
-	        $scope.host = app.host;
-
-	    };
-		$scope.add_buying_order = function(form, myfile){
-        Upload.upload({
-            url: app.host + 'buying/orders/store',
-            data: {
-                user_id: $scope.loginUser.id,
-                style: $scope.order.style_id,
-                ref: $scope.order.ref,
-                pi_date: $scope.order.pi_date,
-                handover_date: $scope.order.handover_date,
-                Gauge: $scope.order.order_gg,
-                yarn_ref_details: $scope.order.yarn_ref,
-                customer: $scope.order.customer,
-                colors: $scope.order.color,
-                qty: $scope.order.order_qty,
-                sizing: $scope.order.sizing,
-                main_label: $scope.order.main_label,
-                hang_tag: $scope.order.hang_tag,
-                colors: $scope.order.colors,
-                customer: $scope.order.customer,
-                contract_weight: $scope.order.contract_weight,
-                sketch: myfile
-            },
-=======
         if(data_type == "date_data")
         {
             var d = new Date(value);
             d = $.datepicker.formatDate('yy-mm-dd', d);
             value = d;
         }
-        
+
         if(value.length == 0)
             value='-';
 
@@ -142,37 +93,42 @@ angular.module('myApp').controller('BuyingOrderController', function($scope, $ht
         $http.get(app.host + 'buying/order/fetchOrderDetails/'+$routeParams.order_id).then(function (response) {
             $scope.order_details = response.data;
             console.log($scope.order_details);
-            
+
         });
 
     };
-    
+
     $scope.add_buying_order = function(form, myfile){
         console.log($scope.order.color)
-    Upload.upload({
-        url: app.host + 'buying/orders/store',
-        data: {
-            user_id: $scope.loginUser.id,
-            style: $scope.order.style_id,
-            ref: $scope.order.ref,
-            pi_date: $scope.order.pi_date,
-            handover_date: $scope.order.handover_date,
-            Gauge: $scope.order.order_gg,
-            yarn_ref_details: $scope.order.yarn_ref,
-            customer: $scope.order.customer,
-            colors: $scope.order.color,
-            qty: $scope.order.order_qty,
-            sizing: $scope.order.sizing,
-            main_label: $scope.order.main_label,
-            hang_tag: $scope.order.hang_tag,
-            colors: $scope.order.colors,
-            customer: $scope.order.customer,
-            contract_weight: $scope.order.contract_weight,
-            sketch: myfile
-        },
->>>>>>> 0ad9bd16f39e053db77a6e9275e06207a360cd97
+        Upload.upload({
+            url: app.host + 'buying/orders/store',
+            data: {
+                user_id: $scope.loginUser.id,
+                style: $scope.order.style_id,
+                ref: $scope.order.ref,
+                pi_date: $scope.order.pi_date,
+                handover_date: $scope.order.handover_date,
+                Gauge: $scope.order.order_gg,
+                yarn_ref_details: $scope.order.yarn_ref,
+                customer: $scope.order.customer,
+                colors: $scope.order.color,
+                qty: $scope.order.order_qty,
+                sizing: $scope.order.sizing,
+                main_label: $scope.order.main_label,
+                hang_tag: $scope.order.hang_tag,
+                colors: $scope.order.colors,
+                customer: $scope.order.customer,
+                contract_weight: $scope.order.contract_weight,
+                sample: myfile.sample,
+                sketch: myfile.image
+            },
         }).then(function (response) {
             $('#add-order-modal').modal('toggle');
+            $http.get(app.host + 'buying/order/fetchOrdersList/'+$scope.loginUser.id+'/'+$scope.loginUser.emp_role).then(function (response) {
+                $scope.num_of_items = 10;
+                $scope.orders = response.data.orders;
+                $scope.reverse = false;
+            });
             $('.top-right').notify({
                 type: 'success',
                 message: { html: '<span class="glyphicon glyphicon-info-sign"></span> <strong>You have successfully add an order.</strong>' },
@@ -181,8 +137,8 @@ angular.module('myApp').controller('BuyingOrderController', function($scope, $ht
             }).show();
             $scope.order = {};
 
-        }, function (response) {console.log(data)
-            $('#add-buyer-modal').modal('toggle');
+        }, function (response) {console.log(response)
+            $('#add-order-modal').modal('toggle');
             $('.top-right').notify({
                 type: 'danger',
                 message: { html: '<span class="glyphicon glyphicon-info-sign"></span> <strong>The operation was unsuccessful.</strong>' },
